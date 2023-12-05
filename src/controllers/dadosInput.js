4b602a7e068aaec50cdb569aeabb8db5a67efe66
Joao Victor <joao.vsantos1@outlook.com>
2023-12-04 23:26:47 -0300

    update dadosInput and malhaMedicao.html

--- src/controllers/dadosInput.js
+++ src/controllers/dadosInput.js
@@ -2,14 +2,13 @@ function calcular() {
     // Obter dados do formulário
     var data = document.getElementById('campo0').value;
     var distanciaEntrePostes = parseFloat(document.getElementById('campo1').value);
-    var larguraVia = document.getElementById('campo2').value;
+    var larguraVia = parseInt(document.getElementById('campo2').value);
     var faixasDeRolamento = document.getElementById('campo3').value;
     var alturaLuminaria = document.getElementById('campo4').value;
     var larguraEstacionamentoA = document.getElementById('campo5').value;
     var larguraEstacionamentoB = document.getElementById('campo6').value;
     var tipoLuminaria = document.getElementById('campo7').value;
 
-    var resultadoCalculadoPoste = calcularDistancias(distanciaEntrePostes);
     // Selecionar a linha pela classe
     var linha = document.querySelector('.inputMed');
 
@@ -29,16 +28,14 @@ function calcular() {
     var linha3 = document.querySelector('.inputEstA')
     linha3.children[0].textContent = "ESTACIONAMENTO A = "+larguraEstacionamentoA.toUpperCase()+ " METROS.";
 
-    var linha4 = document.querySelector('.calculoDist');
+    var linha4 = document.querySelector('.calculoDist')
+    linha4.children[0].textContent = ""
+    linha4.children[1].textContent = 0
+    linha4.children[2].textContent = (distanciaEntrePostes/2)/2
+    linha4.children[3].textContent = distanciaEntrePostes / 2
+    linha4.children[4].textContent = (distanciaEntrePostes / 2) + (distanciaEntrePostes/2)/2
+    linha4.children[5].textContent = distanciaEntrePostes
 
-    // Atualizar os valores nas células da linha
-    linha4.children[0].textContent = "";
-    linha4.children[1].textContent = resultadoCalculadoPoste.distanciaCalculada1;
-    linha4.children[2].textContent = resultadoCalculadoPoste.distanciaCalculada2;
-    linha4.children[3].textContent = resultadoCalculadoPoste.distanciaCalculada3;
-    linha4.children[4].textContent = resultadoCalculadoPoste.distanciaCalculada4;
-    linha4.children[5].textContent = resultadoCalculadoPoste.distanciaCalculada5;
-
-    // Limpar formulário após adicionar à tabela
-    limparFormulario();
+  
 }
+
