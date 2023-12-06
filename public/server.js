794c128b70c5c7dda8a752362a76b4b099ea40a9
Joao Victor <joao.vsantos1@outlook.com>
2023-12-06 14:31:16 -0300

    update index

---
+++ public/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000; // Porta padrão 3000
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'src','view', 'formsCss')));
+app.use( express.static(path.join(__dirname, 'src','view', 'images')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rotas para outras páginas (adapte conforme necessário)
+app.get('/modernizacao', (req, res) => {
+  res.sendFile(path.join(__dirname,'view', 'formsHtml', 'modernizacao.html'));
+});
+
+app.get('/malha-medicao', (req, res) => {
+  res.sendFile(path.join(__dirname,'view', 'formsHtml', 'malhaMedicao.html'));
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
